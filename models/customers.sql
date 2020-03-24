b4e996f6793c7cea5d7a1310e8dff6402a4a1b42
Nam Nguyen <nnguyen83@gmail.com>
2020-03-24 20:38:13 +0000

    Separate customer CTEs into staging tables

--- models/customers.sql
+++ models/customers.sql
@@ -5,19 +5,10 @@
 }}
 
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from `dbt-tutorial`.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 ),
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from `dbt-tutorial`.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 ),
 customer_orders as (
     select
