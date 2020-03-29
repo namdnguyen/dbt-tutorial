29c63dfd26dd8611155b6ad369218fb22833631f
Nam D. Nguyen <nnguyen83@gmail.com>
2020-03-28 22:13:26 -0500

    Convert raw data references to sources

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,4 +2,4 @@ select
     id as customer_id,
     first_name,
     last_name
-from `dbt-tutorial`.jaffle_shop.customers
+from {{ source('shop', 'customers')}}
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,4 +3,4 @@ select
     user_id as customer_id,
     order_date,
     status
-from `dbt-tutorial`.jaffle_shop.orders
+from {{ source('shop', 'orders')}}
