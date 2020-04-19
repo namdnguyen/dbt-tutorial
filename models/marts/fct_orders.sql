c227f248b81dcede0d69c96b3d5c2800f65356dc
namdnguyen <nnguyen83@gmail.com>
2020-04-18 22:58:44 -0500

    Calculate payment method totals for fct_orders

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -4,6 +4,8 @@
   )
 }}
 
+{% set payment_methods = ["bank_transfer", "coupon", "credit_card", "gift_card"] %}
+
 WITH
   orders AS (
     SELECT * FROM {{ ref('stg_orders') }}
@@ -13,14 +15,20 @@ WITH
   ),
   amount_totals AS (
     SELECT order_id,
-           SUM(amount) AS order_amount
+           {% for payment_method in payment_methods %}
+           SUM(CASE WHEN payment_method = '{{payment_method}}' THEN amount END) AS {{payment_method}}_amount,
+           {% endfor %}
+           SUM(amount) AS total_amount
       FROM payments
      GROUP BY 1
     ),
   final AS (
     SELECT orders.order_id,
            customer_id,
-           order_amount,
+           {% for payment_method in payment_methods %}
+           {{payment_method}}_amount,
+           {% endfor %}
+           total_amount,
            order_date,
            status
       FROM orders
